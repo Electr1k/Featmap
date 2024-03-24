a4f44cb22c983fdf64c933653e32b62e1d7c11d8
Electr1k <gynter37@gmail.com>
2024-03-24 22:48:22 +0300

    added navigation graph

--- app/src/main/java/ru/trifonov/featmap/MainActivity.kt
+++ app/src/main/java/ru/trifonov/featmap/MainActivity.kt
@@ -2,10 +2,20 @@ package ru.trifonov.featmap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import ru.trifonov.featmap.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
+    private lateinit var mNavController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+        mNavController = findNavController(R.id.nav_host_fragment_activity_bottom_navigation)
     }
 }
