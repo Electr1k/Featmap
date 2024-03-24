7b6b0a4518f1921b200289b7d450b3d2b0202389
Electr1k <gynter37@gmail.com>
2024-03-24 19:24:39 +0300

    initaial commit

---
+++ app/src/main/java/ru/trifonov/featmap/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.trifonov.featmap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
